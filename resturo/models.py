d3dfb89fd7eafdd5dfd41a9be0a7e27c8607b2fe
Ivo van der Wijk <github@in.m3r.nl>
2015-10-05 13:57:48 +0200

    resolve path using attribute access; resolve model using call

--- resturo/models.py
+++ resturo/models.py
@@ -6,10 +6,27 @@ from django.contrib.auth import get_user_model
 from django.core.exceptions import ImproperlyConfigured
 
 class ModelResolver(object):
+    def __call__(self, name):
+        model_path = getattr(self, name)
+
+        try:
+            app_label, model_class_name = model_path.split('.')
+        except ValueError:
+            raise ImproperlyConfigured(
+                "{0} must be of the form 'app_label.model_name'").format(name)
+
+        model = get_model(app_label, model_class_name)
+        if model is None:
+            raise ImproperlyConfigured(
+                "{0} refers to model '{1}' that has not been "
+                "installed".format(name, model_path))
+
+        return model
+
     def __getattr__(self, name):
         # resolveclass
         if name == 'User':
-            model = get_user_model()
+            model = settings.AUTH_USER_MODEL
         else:
             try:
                 model_path = settings.MODELS[name]
@@ -17,17 +34,19 @@ class ModelResolver(object):
                 raise ImproperlyConfigured(
                     "no MODELS have been configured, {0} can't be resolved").format(name)
 
-            try:
-                app_label, model_class_name = model_path.split('.')
-            except ValueError:
-                raise ImproperlyConfigured(
-                    "{0} must be of the form 'app_label.model_name'").format(name)
+            model = model_path
 
-            model = get_model(app_label, model_class_name)
-            if model is None:
-                raise ImproperlyConfigured(
-                    "{0} refers to model '{1}' that has not been "
-                    "installed".format(name, model_path))
+            #try:
+            #    app_label, model_class_name = model_path.split('.')
+            #except ValueError:
+            #    raise ImproperlyConfigured(
+            #        "{0} must be of the form 'app_label.model_name'").format(name)
+
+            #model = get_model(app_label, model_class_name)
+            #if model is None:
+            #    raise ImproperlyConfigured(
+            #        "{0} refers to model '{1}' that has not been "
+            #        "installed".format(name, model_path))
 
         return model
 

efddeb2bfa969e5b8bde52b8443164528900a7f6
Ivo van der Wijk <github@in.m3r.nl>
2015-10-09 12:11:01 +0200

    depend on factory boy; pep8 fix

--- resturo/urls.py
+++ resturo/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url
 
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'self', views.UserSelfView.as_view(), name='resturo_user_self'),
     url(r'', views.UserCreateView.as_view(), name='resturo_user_create'),
 )

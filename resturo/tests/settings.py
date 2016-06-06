6f321f79cac2ae3bbd21f617fb1ca7220e64e99a
Ivo van der Wijk <github@in.m3r.nl>
2016-06-06 14:38:54 +0200

    cleanup

--- resturo/tests/settings.py
+++ resturo/tests/settings.py
@@ -33,15 +33,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware'
 ]
 
-TEMPLATE_CONTEXT_PROCESSORS = [
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.media',
-    'django.core.context_processors.request',
-    'django.contrib.messages.context_processors.messages',
-    'django.core.context_processors.static',
-]
-
 MODELS = {
     'Organization': 'tests.Organization',
     'Membership': 'tests.Membership'
@@ -50,12 +41,8 @@ MODELS = {
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.humanize',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.sitemaps',
-    'django.contrib.staticfiles',
 
     'resturo.tests',
     'resturo'

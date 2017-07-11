91f5005b03b79fa15c0a6a3d9e6a1dd415544404
Ivo van der Wijk <github@in.m3r.nl>
2017-07-11 15:40:48 +0200

    remove print()'s

--- resturo/signals.py
+++ resturo/signals.py
@@ -17,45 +17,45 @@ user_email_invite = Signal(providing_args=["Invite"])
 @receiver(user_rest_created, dispatch_uid="resturo.signals.send_welcome_mail")
 def send_welcome_mail(sender, user, **kwargs):
     if getattr(settings, "RESTURO_SEND_WELCOME", False):
-        print("User created")
+        pass
 
 
 @receiver(user_rest_emailchange,
           dispatch_uid="resturo.signals.send_email_verification")
 def send_email_verification(sender, user, **kwargs):
     if getattr(settings, "RESTURO_VERIFY_EMAIL", False):
-        print("Email changed")
+        pass
 
 
 @receiver(user_email_verified,
           dispatch_uid="resturo.signals.handle_email_verified")
 def handle_email_verified(sender, user, **kwargs):
     if getattr(settings, "RESTURO_VERIFY_EMAIL", False):
-        print("Email verified")
+        pass
 
 
 @receiver(user_password_reset,
           dispatch_uid="resturo.signals.send_password_mail")
 def send_password_mail(sender, user, **kwargs):
     if getattr(settings, "RESTURO_SEND_PASSWORDRESET", False):
-        print("User password reset")
+        pass
 
 
 @receiver(user_password_confirm,
           dispatch_uid="resturo.signals.send_password_confirm_mail")
 def send_password_confirm_mail(sender, user, **kwargs):
     if getattr(settings, "RESTURO_SEND_PASSWORDRESET", False):
-        print("User password confirm")
+        pass
 
 
 @receiver(user_existing_invite,
           dispatch_uid="resturo.signals.send_invite_user")
 def send_invite_user(sender, user, **kwargs):
     if getattr(settings, "RESTURO_SEND_INVITE", False):
-        print("User invite")
+        pass
 
 
 @receiver(user_email_invite, dispatch_uid="resturo.signals.send_invite_email")
 def send_invite_email(sender, user, **kwargs):
     if getattr(settings, "RESTURO_SEND_INVITE", False):
-        print("Email invite")
+        pass
